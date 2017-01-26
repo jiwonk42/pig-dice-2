9d15b0d00a93914538b2130473107702c08d8262
Jiwon Kang and sandro mijatovic <sandro@>
2017-01-26 11:55:40 -0800

    revamp js using constructors,objects, and prototype methods

--- js/scripts.js
+++ js/scripts.js
@@ -1,15 +1,53 @@
 // Business Logic
-function Score(currentScore, totalScore) {
-  this.currentScore = currentScore;
-  this.totalScore = totalScore;
+function Player() {
+  this.runningTotal = 0;
+  this.totalScore = 0;
 }
 
+Player.prototype.holdScore = function() {
+  this.totalScore += this.runningTotal;
+}
 
-// User Interface Logic
+Player.prototype.rollDice = function() {
+  var random = randomNum();
+  if (random > 1) {
+    this.runningTotal += random;
+  } else {
+    alert("You rolled a 1! Player 2's turn!")
+    this.runningTotal = 0;
+  }
+}
+
+function randomNum() {
+  return Math.floor(Math.random() * 6 + 1)
+}
+
 $(document).ready(function() {
-  $("form#pig-dice").submit(function(event) {
-    event.preventDefault();
 
-    
+var player1 = new Player();
+var player2 = new Player();
+
+  // Player 1
+  $("button#roll-btn-1").click(function() {
+    player1.rollDice();
+    $(".current-score-1").text(player1.runningTotal);
+
+  });
+
+  // Player 2
+  $("button#roll-btn-2").click(function() {
+    player2.rollDice();
+    $(".current-score-2").text(player2.runningTotal);
+
+  });
+
+  // Hold Button
+  $("button#hold-btn").click(function(event) {
+    event.preventDefault();
+    player1.holdScore();
+    $(".total-score-1").text(player1.totalScore);
+    player1.runningTotal = 0;
+    $(".current-score-1").text(player1.runningTotal);
+
   });
 });
