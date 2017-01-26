7943126b0a8e80fe6c71e5e4dc82f9bf6d459407
Jiwon Kang and sandro mijatovic <sandro@>
2017-01-26 08:54:20 -0800

    add a constructor called Score in business logic, and document ready function in user interface

---
+++ js/scripts.js
@@ -0,0 +1,15 @@
+// Business Logic
+function Score(currentScore, totalScore) {
+  this.currentScore = currentScore;
+  this.totalScore = totalScore;
+}
+
+
+// User Interface Logic
+$(document).ready(function() {
+  $("form#pig-dice").submit(function(event) {
+    event.preventDefault();
+
+    
+  });
+});
